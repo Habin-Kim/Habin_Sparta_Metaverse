b0587dcc065d21bdb77c214e947f36845cd0b83d
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-05-02 17:26:38 +0900

    add : 이동 구현, 좌우 이동 시 좌우반전, 타일맵
    add 이동 구현, 좌우 이동 시 좌우반전, 타일맵

---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+    // private GameManager gameManager;
+
+    // public void Init(GameManager gameManager)
+    // {
+    //     this.gameManager = gameManager;
+    //     camera = Camera.main;
+    // }
+
+    protected override void HandleAction()
+    {
+      
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+        lookDirection = movementDirection;
+    }
+}
